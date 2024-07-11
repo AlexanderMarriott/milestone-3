959a65167d4d65cbc590134d150327f7c309c4ad
Alexander Marriott <a1exviper@icloud.com>
2024-07-11 17:01:00 +0000

    added initialisation for modals and side nav, added behaviour for modals

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.modal').modal({
+        dismissible: true,
+        onOpenStart: function(modal, trigger) {
+            var content = $(modal).find('.modal-content');
+            var form = content.find('form');
+            form.css('maxHeight', (content.height() - 60) + 'px');
+            form.css('overflow', 'auto');
+        }
+    });
+    $(document).on('click', '.switch-modal', function(e) {
+        e.preventDefault();
+        var targetModal = $(this).attr('href');
+        $('.modal').modal('close');
+        setTimeout(function() {
+            $(targetModal).modal('open');
+        }, 200); 
+    });
+  });
