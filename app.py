f81a8b285fa2b537c29203a05a24500260154df2
Alexander Marriott <a1exviper@icloud.com>
2024-07-11 13:26:52 +0000

    check to see if the app is connected to database

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, flash, render_template, redirect, request, session, url
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from pymongo.errors import ConnectionFailure
 
 if os.path.exists("env.py"):
     import env
@@ -13,5 +14,26 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-
 mongo = PyMongo(app)
+
+def check_mongo_connection():
+    try:
+        mongo.cx.admin.command('ping')
+        print("MongoDB connection successful")
+    except ConnectionFailure:
+        print("MongoDB connection failed")
+        return False
+    return True
+
+if not check_mongo_connection():
+    raise Exception("Failed to connect to MongoDB. Please check your settings.")
+
+@app.route('/')
+def home():
+    return "Hello, Flask with MongoDB!"
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
