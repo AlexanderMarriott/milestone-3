5d3d1afe9e1ab0a407cf4b794279cf0133502cb1
AlexanderMarriott <150162872+AlexanderMarriott@users.noreply.github.com>
2024-07-14 18:02:37 +0100

    added styling for table element

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
+    $('.collapsible').collapsible();
     $('.modal').modal({
         dismissible: false,
         onOpenStart: function(modal, trigger) {
@@ -29,5 +30,28 @@ $(document).ready(function(){
         $profileHeadlineCounter.text(currentLength + '/' + maxLength);
     });
 
-    
+      // Initially disable the edit icon
+      $('.fas.fa-edit').closest('a').addClass('disabled');
+
+      // Function to check and update icons based on selection
+      $('.portfolio .selectable-item').click(function() {
+          // Toggle selected state
+          var isSelected = $(this).data('selected');
+          $(this).data('selected', !isSelected);
+          $(this).toggleClass('selected');
+  
+          // Check if any item is selected
+          var anySelected = $('.portfolio .selectable-item').is(function() {
+              return $(this).data('selected');
+          });
+  
+          // Update icons based on selection
+          if (anySelected) {
+              $('#actionIcon i').removeClass('fa-plus').addClass('fa-trash').css('color', 'red');
+              $('#editIcon').removeClass('disabled').addClass('highlighted');
+          } else {
+              $('#actionIcon i').removeClass('fa-trash').addClass('fa-plus').removeAttr('style');
+              $('#editIcon').addClass('disabled').removeClass('highlighted');
+          }
+      });
   });
